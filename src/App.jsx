ae122ebc1233f2856e267816944f3c0a4edbb8fc
jorneylopez <jorney95@live.com>
2022-12-12 23:03:04 -0600

    Added context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { CotizadorProvider } from "./context/CotizadorProvider"
 import AppSeguro from "./components/AppSeguro"
-
 
 function App() {
   return (
-    <AppSeguro />
+    <CotizadorProvider>
+      <AppSeguro />
+    </CotizadorProvider>
   )
 }
 
